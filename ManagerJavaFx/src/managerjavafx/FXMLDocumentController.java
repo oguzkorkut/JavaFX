c59997d67d57b995561df886b5cf80dd7d95fb8b
Oğuz Korkut <oguzkrkt@gmail.com>
2019-01-10 00:29:00 +0300

    JavaFX butona tıklandığında ilgili ekranın açılması

--- ManagerJavaFx/src/managerjavafx/FXMLDocumentController.java
+++ ManagerJavaFx/src/managerjavafx/FXMLDocumentController.java
@@ -6,16 +6,21 @@
 package managerjavafx;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  *
@@ -27,8 +32,7 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane anc;
     @FXML
     private VBox btnCeasses;
-    @FXML
-    private JFXButton btnHome;
+    
     @FXML
     private JFXButton btnStudents;
     @FXML
@@ -37,6 +41,8 @@ public class FXMLDocumentController implements Initializable {
     private JFXButton btnSettings;
     @FXML
     private JFXButton btnUpdate;
+    @FXML
+    private JFXButton btnDashboard;
     
     
     private void handleButtonAction(ActionEvent event) {
@@ -44,15 +50,15 @@ public class FXMLDocumentController implements Initializable {
        
     }
     
-    private void handleButtonClicks(MouseEvent event) {
+    private void handleButtonClicks(ActionEvent event) {
         System.out.println("You clicked me!");
         
-        if(event.getSource() == btnHome){
-            
+        if(event.getSource() == btnDashboard){
+            loadStages("/managerjavafx/Dashboard.fxml");
         } else if(event.getSource() == btnStudents){
-            
+             loadStages("/managerjavafx/Students.fxml");
         } else if(event.getSource() == btnTimetable){
-            
+             loadStages("/managerjavafx/Students.fxml");
         }
        
     }
@@ -66,5 +72,27 @@ public class FXMLDocumentController implements Initializable {
     private void click(MouseEvent event) {
         System.out.println("FXMLDocumentController.click()");
     }
+
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleButtonClicks(MouseEvent event) {
+    }
     
+    
+    private void loadStages(String fxml){
+        try{
+           Parent root = FXMLLoader.load(getClass().getResource(fxml));
+           
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+          
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
